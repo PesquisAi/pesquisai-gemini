dc6f6a2440e0f9b5e160695af45c801cf22aa827
GabiHert <hertergabriel4@gmail.com>
2024-05-22 19:01:59 -0300

    feat: allow null research_id

--- internal/delivery/dtos/request_dto.go
+++ internal/delivery/dtos/request_dto.go
@@ -1,7 +1,7 @@
 package dtos
 
 type GeminiRequest struct {
-	ResearchId  *string         `json:"research_id,omitempty" validate:"uuid"`
+	ResearchId  *string         `json:"research_id,omitempty" validate:"omitempty,uuid"`
 	RequestId   *string         `json:"request_id" validate:"required,uuid"`
 	Question    *string         `json:"question" validate:"required,min=1"`
 	OutputQueue *string         `json:"output_queue" validate:"required,min=1"`
